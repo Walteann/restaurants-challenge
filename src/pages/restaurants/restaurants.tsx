609a6756c41fa585cc21db16d528565ac8e8e44d
Walteann <walteann3@gmail.com>
2021-11-23 22:18:46 -0300

    feat: commit inicial

---
+++ src/pages/restaurants/restaurants.tsx
@@ -0,0 +1,15 @@
+import './restaurants.scss';
+
+export const Restaurants = () => {
+    return (
+        <>
+            <h1>Restaurants</h1>
+
+            <p className="fontRegular">Fonte Regular</p>
+            <p className="fontMedium">Fonte Medium</p>
+            <p className="fontSemibold">Fonte SemiBold</p>
+            <p className="fontBold">Fonte Bold</p>
+
+        </>
+    )
+}
